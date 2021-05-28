8cecfef8049217761c13970bd338aa1dc10d4ec8
japneet0411 <60810523+japneet0411@users.noreply.github.com>
2021-05-29 02:20:53 +0530

    Styling done for pages

--- client/src/sections/contactUs/contactUs.jsx
+++ client/src/sections/contactUs/contactUs.jsx
@@ -1,44 +1,53 @@
-import React from 'react';
-import './contactUs.scss';
-import './contactUs.css';
-import Navbar from './../../components/Navbar/navbarHome';
-import { Button, Form } from 'react-bootstrap';
+import React from "react";
+import "./contactUs.scss";
+import "./contactUs.css";
+import Navbar from "./../../components/Navbar/navbarHome";
+import { Button, Form } from "react-bootstrap";
 function Contact() {
-	return (
-		<div className='contact'>
-			<Navbar />
-			<div id='head'>
-				<p class='text'>Contact Us</p>
-			</div>
-			<div id='body'>
-				Having any Queries?<br></br>
-				Provide your Details below, we will reach to you Asap!<br></br>
-				<Form>
-					<Form.Group controlId='formBasicName'>
-						<Form.Label>Name</Form.Label>
-						<Form.Control type='text' placeholder='Enter Name' />
-					</Form.Group>
+  return (
+    <div className="contact">
+      <Navbar />
+      <div className="contact-Us">
+        <p className="header p-3">Contact Us</p>
+        <hr />
+        Having any Queries?<br></br>
+        Provide your Details below, we will reach to you Asap!
+        <br />
+        <br />
+        <Form>
+          <Form.Group controlId="formBasicName">
+            <Form.Label className="Label">Name</Form.Label>
+            <Form.Control
+              className="Input"
+              type="text"
+              placeholder="Enter Name"
+            />
+          </Form.Group>
 
-					<Form.Group controlId='formBasicEmail'>
-						<Form.Label>Email address</Form.Label>
-						<Form.Control type='email' placeholder='Enter email' />
-						<Form.Text className='text-muted'>
-							We'll never share your data with anyone else.
-						</Form.Text>
-					</Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label className="Label">Email address</Form.Label>
+            <Form.Control
+              className="Input"
+              type="email"
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your data with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-					<Form.Group controlId='ControlTextarea1'>
-						<Form.Label>Feedback/Queries</Form.Label>
-						<Form.Control as='textarea' rows={3} />
-					</Form.Group>
+          <Form.Group controlId="ControlTextarea1">
+            <Form.Label className="Label">Feedback/Queries</Form.Label>
+            <Form.Control className="Input-Area" as="textarea" rows={3} />
+          </Form.Group>
 
-					<Button variant='primary' type='submit'>
-						Submit
-					</Button>
-				</Form>
-			</div>
-		</div>
-	);
+          <Button variant="dark" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- client/src/sections/signUp/signUp.jsx
+++ client/src/sections/signUp/signUp.jsx
@@ -1,104 +1,106 @@
-import React, { useState } from 'react';
-import './signUp.css';
-import './signUp.scss';
-import { Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import { useHistory } from 'react-router-dom';
-import Navbar from './../../components/Navbar/navbarHome';
+import React, { useState } from "react";
+import "./signUp.css";
+import "./signUp.scss";
+import { Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useHistory } from "react-router-dom";
+import Navbar from "./../../components/Navbar/navbarHome";
+import { Container } from "react-bootstrap";
 
 function SignUp() {
-	const [username, setUsername] = useState('');
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-	const history = useHistory();
+  const history = useHistory();
 
-	const registerUser = (e) => {
-		e.preventDefault();
-		axios
-			.post('http://localhost:5000/signUp', {
-				username: username,
-				email: email,
-				password: password,
-				confirmPassword: confirmPassword,
-			})
-			.then((response) => {
-				Swal.fire(response.data.message).then((result) => {
-					if (result.isConfirmed) {
-						if (response.data.message === 'Successfully created account') {
-							console.log('created account');
-							history.push('/login');
-						} else {
-							window.location.reload();
-						}
-					}
-				});
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
+  const registerUser = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/signUp", {
+        username: username,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      })
+      .then((response) => {
+        Swal.fire(response.data.message).then((result) => {
+          if (result.isConfirmed) {
+            if (response.data.message === "Successfully created account") {
+              console.log("created account");
+              history.push("/login");
+            } else {
+              window.location.reload();
+            }
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-	return (
-		<div className='parent'>
-			<Navbar />
-			<Form className='Form'>
-				<center>
-					<h2>SIGN UP</h2>
-				</center>
-				<Form.Group controlId='formGroupUsername'>
-					<Form.Label className='label pos'>Username</Form.Label>
-					<Form.Control
-						className='box'
-						type='text'
-						placeholder='Enter username'
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-				</Form.Group>
-				<Form.Group controlId='formGroupEmail'>
-					<Form.Label className='label pos'>Email address</Form.Label>
-					<Form.Control
-						className='box'
-						type='email'
-						placeholder='Enter email'
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</Form.Group>
+  return (
+    <div className="parent">
+      <Navbar />
+      <Form className="Form">
+        <center>
+          <h2 className="sign">SIGN UP</h2>
+        </center>
+        <Form.Group controlId="formGroupUsername">
+          <Form.Label className="labels">Username</Form.Label>
+          <Form.Control
+            className="box"
+            type="text"
+            placeholder="Enter username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formGroupEmail">
+          <Form.Label className="labels">Email address</Form.Label>
+          <Form.Control
+            className="box"
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
 
-				<Form.Group controlId='formGroupPassword'>
-					<Form.Label className='label'>Password</Form.Label>
-					<Form.Control
-						className='box'
-						type='password'
-						placeholder='Password'
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</Form.Group>
+        <Form.Group controlId="formGroupPassword">
+          <Form.Label className="labels">Password</Form.Label>
+          <Form.Control
+            className="box"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
 
-				<Form.Group controlId='formGroupConfirmPassword'>
-					<Form.Label className='label pos2'>Confirm Password</Form.Label>
-					<Form.Control
-						className='box'
-						type='password'
-						placeholder='Confirm password'
-						onChange={(e) => setConfirmPassword(e.target.value)}
-					/>
-				</Form.Group>
-				<p className='text-muted text'>
-					Already have an account yet? <Link to='/login'>Sign In</Link>
-				</p>
-				<button
-					className='btns'
-					onClick={(e) => {
-						registerUser(e);
-					}}>
-					Sign Up
-				</button>
-			</Form>
-		</div>
-	);
+        <Form.Group controlId="formGroupConfirmPassword">
+          <Form.Label className="labels">Confirm Password</Form.Label>
+          <Form.Control
+            className="box"
+            type="password"
+            placeholder="Confirm password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+        <p className="text-muted text">
+          Already have an account yet? <Link to="/login">Sign In</Link>
+        </p>
+        <button
+          className="btns"
+          onClick={(e) => {
+            registerUser(e);
+          }}
+        >
+          Sign Up
+        </button>
+      </Form>
+    </div>
+  );
 }
 export default SignUp;
