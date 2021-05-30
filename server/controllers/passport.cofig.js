6de892fc55088276d610ca7bd2df9191facb5749
aayuv17 <aayuv17@gmail.com>
2021-05-30 15:50:50 +0530

    fixed salting for database

--- server/controllers/passport.cofig.js
+++ server/controllers/passport.cofig.js
@@ -1,50 +1,56 @@
-const passport = require('passport');
+const passport = require("passport");
 
-const bcrypt = require('bcryptjs');
-const LocalStrategy = require('passport-local').Strategy;
+const crypto = require("crypto");
+const LocalStrategy = require("passport-local").Strategy;
 
-import { userAuthModel } from './../models/userAuth';
+import { userAuthModel } from "./../models/userAuth";
 
-console.log('In passport.js');
+console.log("In passport.js");
 
 passport.serializeUser(function (user, done) {
-	done(null, user.username);
+  done(null, user.username);
 });
 
 passport.deserializeUser(function (username, done) {
-	User.findOne({ username: username }, function (err, user) {
-		done(err, user);
-	});
+  User.findOne({ username: username }, function (err, user) {
+    done(err, user);
+  });
 });
 
 passport.use(
-	new LocalStrategy(
-		{
-			usernameField: 'username',
-		},
-		function (username, password, done) {
-			userAuthModel.findOne({ username: username }, function (err, user) {
-				console.log(user);
-				// If Error Occurs, Return the Error
-				if (err) {
-					return done(err);
-				}
-				// User Not Found
-				if (!user) {
-					return done(null, false, {
-						message: 'Incorrect Username or Password',
-					});
-				}
-				// User Entered Wrong Password
-				if (!(bcrypt.hashSync(password, user.salt) == user.password)) {
-					return done(null, false, {
-						message: 'Incorrect Username or Password',
-					});
-				}
-				//console.log(req.user);
-				// Everything's Fine
-				return done(null, user);
-			});
-		}
-	)
+  new LocalStrategy(
+    {
+      usernameField: "username",
+    },
+    function (username, password, done) {
+      userAuthModel.findOne({ username: username }, function (err, user) {
+        console.log(user);
+        // If Error Occurs, Return the Error
+        if (err) {
+          return done(err);
+        }
+        // User Not Found
+        if (!user) {
+          return done(null, false, {
+            message: "Incorrect Username or Password",
+          });
+        }
+        // User Entered Wrong Password
+        if (
+          !(
+            crypto
+              .pbkdf2Sync(password, user.salt, 1000, 64, "sha512")
+              .toString("hex") == user.password
+          )
+        ) {
+          return done(null, false, {
+            message: "Incorrect Username or Password",
+          });
+        }
+        //console.log(req.user);
+        // Everything's Fine
+        return done(null, user);
+      });
+    }
+  )
 );
